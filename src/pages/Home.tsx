6b78e837e6639f7b2ab76dab5d96e3b100471307
Mohammad Mhameed <mohmed6535@gmail.com>
2024-07-04 18:41:05 +0300

    integrate zustand state management

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import useHomeStore from './../stores/homeStore';
 
 const Home: React.FC = () => {
+  const homeStore = useHomeStore();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle className='text-blue'>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
+
           </IonToolbar>
         </IonHeader>
+        <IonContent className='ion-padding'>
+          <h1 >{homeStore.count}</h1>
+          <IonButton onClick={homeStore.increment}>
+            Click me
+          </IonButton>
+        </IonContent>
         <ExploreContainer />
       </IonContent>
     </IonPage>
